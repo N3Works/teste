4fe81c1f6149da3a86bf932672aff3614e40d2f0
Limazix <blimacardoso@gmail.com>
2019-03-16 17:20:47 -0300

    Executing search at zendesk webhook function

--- functions/specs/zendesk-webhook/zendesk-webhook.spec.js
+++ functions/specs/zendesk-webhook/zendesk-webhook.spec.js
@@ -6,9 +6,11 @@ const zendeskWebhook = require('../../zendesk-webhook/zendesk-webhook');
 describe('Zendesk Webhook', () => {
     describe('Given an update request is fired', () => {
         it('should get the zendesk credentials', async () => {
-            const spy = jest.spyOn(zendeskWebhook, 'getCredentials');
+            const spyGetCredentials = jest.spyOn(zendeskWebhook, 'getCredentials');
+            const spySearch = jest.spyOn(zendeskWebhook, 'search');
             await searchChanges({data:{ attributes: "testing pubsub ..."}});
-            expect(spy).toBeCalled();
+            expect(spyGetCredentials).toBeCalled();
+            expect(spySearch).toBeCalled();
         });
     });
 });
--- functions/zendesk-webhook/zendesk-webhook.js
+++ functions/zendesk-webhook/zendesk-webhook.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const axios = require('axios');
+const ZendeskAPI = require('@kiina/zendesk-api');
+
 /**
  * @name getCredentials
  * @async
@@ -14,6 +17,31 @@ exports.getCredentials = async (database) => {
 };
 
 /**
+ * @name search
+ * @async
+ * @description Method used to run a given query into Zendesk database and return its results
+ * @param {Object} config Zendesk credentials and query
+ * @returns {Array<Objects>} It returns the list of objects found into the Zendesk database
+ * @todo It should use the zendesk-api for Kiina projects @see `@kiina/zendesk-api`
+ */
+exports.search = async (config) => {
+    const url = `${config.api}/search.json?query=${config.query}`;
+    const response = await axios.request({
+        url,
+        method: 'GET',
+        auth: {
+            username: config.username,
+            password: config.password
+        },
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${config.accessToken}`
+        }
+    });
+    return response.data.results;
+};
+
+/**
  * @name handler
  * @async
  * @description Method used to handle the Zendesk Webhook opperation
@@ -22,5 +50,6 @@ exports.getCredentials = async (database) => {
  */
 exports.handler = async (data, database) => {
     const config = await this.getCredentials(database);
+    const result = await this.search(config.crm);
     return null;
 };
