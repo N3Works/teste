d2b6ec84f9b09c366fff999e7c0757414ffc2ca6
Limazix <blimacardoso@gmail.com>
2019-03-18 23:43:10 -0300

    Implemanting method to registry a new function to the current module

--- functions/index.js
+++ functions/index.js
@@ -12,31 +12,42 @@ if (!process.env.FIREBASE_CONFIG) {
     });
 } else admin.initializeApp();
 
-const search = require('./search/search');
-exports.zendeskSearch = functions.pubsub.topic(getTrigger('zendesk-search')).onPublish(async event => {
-    try {
-        return await search.handler({
-            data: event.json,
-            database: admin.database()
-        });
-    } catch (error) {
-        console.error(error);
-        event.json.error = error;
-        await publishEvent(event.json, getFailTopic('zendesk-search'));
-    }
-});
+/**
+ * @name registryFunction
+ * @description Method used to registry a new function to the current module
+ * @param {Object} setup Object with the informations to setup a new function
+ * @param {Object} setup.opperator Object reference for the module that holds the function handler (must inplement one method named handler)
+ * @param {string} setup.functionName String that represents the function name to be registry at Firebase
+ * @param {string} setup.trigger String that holds the function trigger name
+ */
+const registryFunction = ({opperator, functionName, trigger}) => {
+    exports[functionName] = functions.pubsub.topic(getTrigger(trigger)).onPublish(async event => {
+        try {
+            return await opperator.handler({
+                config: event.json.config,
+                data: event.json.data,
+                database: admin.database()
+            });
+        } catch (error) {
+            console.error(error);
+            event.json.error = error;
+            await publishEvent(event.json, getFailTopic(trigger));
+        }
+    });
+};
 
-const getTicketComments = require('./get-ticket-comments/get-ticket-comments');
-exports.zendeskGetTicketComments = functions.pubsub.topic(getTrigger('zendesk-get-ticket-comments')).onPublish(async event => {
-    try {
-        return await getTicketComments.handler({
-            config: event.json.config,
-            data: event.json.data,
-            databese: admin.database()
-        });
-    } catch (error) {
-        console.error(error);
-        event.json.error = error;
-        await publishEvent(event.json, getFailTopic('zendesk-get-ticket-comments'));
-    }
+registryFunction({
+    opperator: require('./search/search'),
+    functionName: 'zendeskSearch',
+    trigger: 'zendesk-search'
+});
+registryFunction({
+    opperator: require('./get-ticket-comments/get-ticket-comments'),
+    functionName: 'zendeskGetTicketComments',
+    trigger: 'zendesk-get-ticket-comments'
+});
+registryFunction({
+    opperator: require('./update-ticket/update-ticket'),
+    functionName: 'zendeskUpdateTicket',
+    trigger: 'zendesk-update-ticket'
 });
