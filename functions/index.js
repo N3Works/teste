398c3911221fa9cfa6f84f034ee561624c0bbc6f
Limazix <blimacardoso@gmail.com>
2019-03-26 00:19:19 -0300

    Creating listener for zendesk-update-ticket-success

--- functions/index.js
+++ functions/index.js
@@ -32,5 +32,5 @@ const registryListener = (trigger) => {
 };
 
 /** setup */
-const triggers = ['zendesk-search-success', 'zendesk-get-ticket-comments-success', 'df-process-text-success'];
+const triggers = ['zendesk-search-success', 'zendesk-get-ticket-comments-success', 'df-process-text-success', 'zendesk-update-ticket-success'];
 triggers.forEach(trigger => registryListener(trigger));
