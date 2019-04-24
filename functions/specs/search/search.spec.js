41c3817b5aa3054b97ec490761fd61a73f952cad
janaina <jana-costa@outlook.com>
2019-04-24 11:13:29 -0300

    test to get the zendesk all ticket based on the given query

--- functions/specs/search/search.spec.js
+++ functions/specs/search/search.spec.js
@@ -1,20 +1,47 @@
 "use strict";
 
-const { zendeskSearch } = require("../../");
 const { handler } = require("../../search/search");
-const search = require("../../search/search");
+const ZendeskApi = require("@kiina/zendesk-api");
+const utils = require("../../utils");
+
+let database = {
+  ref: jest.fn().mockReturnThis(),
+  once: jest.fn().mockResolvedValue({
+    val: jest.fn().mockReturnValue({
+      crm: {
+        api: "api",
+        username: "user",
+        password: "1234",
+        subdomain: "teste",
+        query: "banana"
+      }
+    })
+  })
+};
+
+jest.mock("@kiina/zendesk-api");
+jest.mock("../../utils");
 
 describe("Zendesk Webhook", () => {
   describe("Given an update request is fired", () => {
-    it("should get the zendesk credentials", async () => {
-      const spyGetCredentials = jest.spyOn(search, "getCredentials");
-      const spySearch = jest.spyOn(search, "search");
-      await zendeskSearch({
-        data: { json: { message: "testing pubsub ..." } }
+    afterEach(() => {
+      jest.resetAllMocks();
+      jest.restoreAllMocks();
+    });
+    it("should get the zendesk all ticket based on the given query", async () => {
+      ZendeskApi.prototype.search = {
+        run: jest.fn().mockResolvedValue([{}])
+      };
+      utils.publishEvent.mockResolvedValue({});
+
+      await handler({
+        data: { message: "testing pubsub ..." },
+        database
       });
-      expect(spyGetCredentials).toBeCalled();
-      expect(spySearch).toBeCalled();
-    }, 10000);
-    it("should return the search", async () => {});
+      expect(database.once).toBeCalledWith("value");
+      expect(ZendeskApi.prototype.search.run).toBeCalledWith({
+        query: "banana"
+      });
+    });
   });
 });
